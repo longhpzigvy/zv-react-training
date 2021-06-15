8f551e4888d2cb1e490113f849ef7a5109cb7e3f
Huynh Kim Phat <hkimphat2012@gmail.com>
2021-06-15 18:04:57 +0700

    Training2: Update task 3

--- Training2/task3/src/Component/Counter.js
+++ Training2/task3/src/Component/Counter.js
@@ -3,18 +3,21 @@ import { useState, useEffect } from "react";
 const Counter = (props) => {
     const [counter, setCounter] = useState(props.num);
     const [stop, setStop] = useState(false);
-
     useEffect(() => {
-        const interval = setInterval(() => {
-            if (counter === 0 || stop) {
-                return;
-            }
-            setCounter((prevCounter) => prevCounter - 1);
-        }, 1000);
-
-        return () => clearInterval(interval);
-    }, [counter, stop]);
-
+        setCounter(counter);
+        if (counter === 0) {
+            setStop(true);
+        }
+    }, [counter]);
+    useEffect(() => {
+        let intervalId;
+        if (stop === false) {
+            intervalId = setInterval(() => {
+                setCounter((prevCounter) => prevCounter - 1);
+            }, 1000);
+        }
+        return () => clearInterval(intervalId);
+    }, [stop]);
     return (
         <>
             <p>{counter}</p>
