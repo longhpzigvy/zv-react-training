324b8959a06d884f485f9417305200c6b82c5e73
Huynh Kim Phat <hkimphat2012@gmail.com>
2021-06-17 10:33:53 +0700

    Training4: Update Code Task 1

--- Training4/task1/src/App.js
+++ Training4/task1/src/App.js
@@ -1,33 +1,34 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import "./App.css";
+import _ from "lodash";
 import QuoteItem from "./Component/QuoteItem";
 const fetchRandomQuote = () => {
     return axios
         .get("https://official-joke-api.appspot.com/random_joke")
         .then((res) => {
-            console.log(res);
             return res;
-        })
-        .catch((err) => {
-            console.log(err);
         });
 };
-
 const App = () => {
+    const clickRef = useRef();
     const [loadedQuotes, setLoadedQuotes] = useState([]);
     useEffect(() => {
+        clickRef.current = _.debounce(() => delayedHandleChange(), 500);
         fetchRandomQuote().then((randomData) => {
             setLoadedQuotes([randomData.data]);
         });
     }, []);
-    const clickHandler = () => {
+    const delayedHandleChange = () => {
         fetchRandomQuote().then((random) => {
             setLoadedQuotes((prevState) => {
                 return [...prevState, random.data];
             });
             console.log(loadedQuotes);
         });
+    };
+    const clickHandler = () => {
+        clickRef.current();
     };
     return (
         <>
@@ -49,3 +50,56 @@ const App = () => {
 };
 
 export default App;
+
+// function useDebounce(text, delay) {
+//   delay = delay || 1000;
+//   const [debounced, setDebounced] = useState(text);
+//   useEffect(() => {
+//     const handler = setTimeout(() => {
+//       setDebounced(text);
+//     }, delay);
+//     return () => {
+//       clearTimeout(handler);
+//     };
+//   }, [text, delay]);
+//   return debounced;
+// }
+// export default function JokeAPI() {
+//   // state
+//   const [joke, setJoke] = useState([]);
+//   const [showJoke, setShowJoke] = useState(false);
+//   const debounceVal = useDebounce(showJoke);
+//   console.log(debounceVal)
+//   const getAPI = () => {
+//     axios
+//       .get("https://official-joke-api.appspot.com/jokes/random")
+//       .then((res) => {
+//         console.log("setJoke");
+
+//         setJoke(res.data);
+//       });
+//   };
+
+//   const delayedHandleChange = debounce(() => getAPI(), 500);
+
+//   return (
+//     <Fragment>
+//       <h2> {joke.setup ? joke.setup : <Fragment />}</h2>
+//       <div className="joke">
+//         <p className="textarea" style={{ color: "tomato" }}>
+//           {" "}
+//           {joke.punchline ? joke.punchline : "\nJoke In here"} 😎
+//         </p>
+//       </div>
+//       <div>
+//         <button
+//           onClick={() => {
+//             delayedHandleChange();
+//           }}
+//         >
+//           Click here to joke
+//         </button>
+//       </div>
+//     </Fragment>
+//   );
+// }
