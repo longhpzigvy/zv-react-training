b11a731d5c3fbaffc2668a3a5b7d3635b9c5b242
thongle12 <thonglm@zigvy.com>
2021-06-23 15:08:52 +0700

    training4_thong

---
+++ Training4/training4/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Task1 from './Task1'
+import Home from './Task2/pages/Home'
+
+function App() {
+  return (
+    <>
+    
+    {/* <Task1/> */}
+
+    <Home/>
+    
+     </>
+  );
+}
+
+export default App;
