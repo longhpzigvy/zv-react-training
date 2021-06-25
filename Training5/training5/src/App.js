ea8a87b10cb0eaf9404a33a26c817bec0a2b1792
thongle12 <thonglm@zigvy.com>
2021-06-25 17:20:55 +0700

    task 1 training 5

--- Training5/training5/src/App.js
+++ Training5/training5/src/App.js
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Task1/pages/Home.jsx';
-import Draft from './Task1/pages/Draft.jsx';
 
 function App() {
   return (
     <div>
       <Home/>
-      {/* <Draft/> */}
     </div>
   );
 }
