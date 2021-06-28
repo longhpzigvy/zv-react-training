666b1a1a704537b1d784b2bfbe6282294563d89e
HKP <kimphat.devcs@gmail.com>
2021-06-28 16:02:52 +0700

    Init Project

--- Training5/manageapp/src/Pages/Home/DashBoard.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function DashBoard() {
-    return (
-        <div style={{ textAlign: "center", width: "100%" }}>Welcome Home</div>
-    );
-}
--- Training5/manageapp/src/index.js
+++ Training5/manageapp/src/index.js
@@ -1,20 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
-import { Provider } from "react-redux";
-import rootSaga from "./redux/sagas/rootSaga";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistor, sagaMiddleware } from "./redux/store/index";
-import { store } from "./redux/store/index";
 
-sagaMiddleware.run(rootSaga);
-
-ReactDOM.render(
-    <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-            <App />
-        </PersistGate>
-    </Provider>,
-    document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
